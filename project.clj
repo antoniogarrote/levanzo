f2e4aa34375267bcd0db2ba5bb23ecfaf9b168e1
Antonio Garrote <antoniogarrote@gmail.com>
2016-12-31 13:13:24 +0100

    Adding travis.yml

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :monkeypatch-clojure-test false
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/test.check "0.9.0" :scope "test"]])

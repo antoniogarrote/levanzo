c92236d2ffb5f29d72839887f0dbc0aed2dce965
Antonio Garrote <antoniogarrote@gmail.com>
2017-01-07 14:37:31 +0000

    Generators and tests for whole API validation

--- src/levanzo/schema.clj
+++ src/levanzo/schema.clj
@@ -1,4 +1,6 @@
 (ns levanzo.schema
+  "Functions to validate a JSON-LD payload based on the Hydra classes definitions of an ApiDocumentation.
+   Functions in this namespaces assumes payloads will be expanded JSON-LD payloads."
   (:require [levanzo.hydra :as hydra]
             [levanzo.spec.jsonld :as jsonld-spec]
             [levanzo.namespaces :refer [xsd prefix-for-ns]]
@@ -8,16 +10,22 @@
             [clojure.spec.gen :as gen]
             [clojure.test.check.generators :as tg]))
 
+;; Definitions around validation errors reporting data
 (s/def ::error-type keyword?)
 (s/def ::error-cause any?)
 (s/def ::error-message string?)
 (s/def ::ValidationError (s/keys :req-un [::error-type ::error-cause ::error-message]))
 (defrecord ValidationError [error-type error-cause error-message])
 
-(s/def ::predicate (s/fspec :args (s/cat :jsonld ::jsonld-spec/expanded-jsonld)
+(s/def ::ValidationsMap (s/with-gen
+                          (s/map-of ::jsonld-spec/uri ::predicate)
+                          #(tg/return {})))
+(s/def ::predicate (s/fspec :args (s/cat :validations-map ::ValidationsMap
+                                         :jsonld ::jsonld-spec/expanded-jsonld)
                             :ret (s/nilable ::ValidationError)))
 
 
+;; is something a validation error?
 (s/fdef invalid?
         :args (s/cat :maybe-error (s/alt :non-error  any?
                                          :validation-error ::ValidationError))
@@ -27,15 +35,22 @@
   [maybe-error]
   (instance? ValidationError maybe-error))
 
-(defn xsd-uri? [t]
+;; validation predicates
+
+(defn xsd-uri?
+  "Checks if t is XSD URI encoded in a string"
+  [t]
   (and (string? t)
        (string/starts-with? t (prefix-for-ns "xsd"))))
+
 
 (s/fdef check-xsd-range
         :args (s/cat :range ::jsonld-spec/datatype
                      :value ::jsonld-spec/jsonld-literal)
         :ret boolean?)
-(defn check-xsd-range [range value]
+(defn check-xsd-range
+  "Checks that the value of JSON-LD property is within the XSD range of a property"
+  [range value]
   (condp = range
     (xsd "string") (string? (get value "@value"))
     (xsd "decimal") (int? (get value "@value"))
@@ -43,22 +58,27 @@
     (xsd "boolean") (boolean? (get value "@value"))
     (throw (Exception. (str "Unknown/not implemented xsd validation for type " range)))))
 
+
 (s/fdef check-range
-        :args (s/with-gen (s/cat :api ::hydra/ApiDocumentation
+        :args (s/with-gen (s/cat :api ::ValidationsMap
                                  :range ::jsonld-spec/uri
                                  :value (s/or
                                          :nested ::jsonld-spec/expanded-jsonld
                                          :literal ::jsonld-spec/jsonld-literal))
-                #(tg/tuple (s/gen ::hydra/ApiDocumentation)
+                #(tg/tuple (tg/return {})
                            (s/gen ::jsonld-spec/datatype)
                            (s/gen ::jsonld-spec/jsonld-literal)))
-        :ret boolean?)
-(defn check-range [api range value]
+        :ret (s/nilable ::ValidationError))
+(defn check-range
+  "Checks that one JSON-LD property value matches a particular range"
+  [api-validations range value]
   (if (xsd-uri? range)
-    (check-xsd-range range value)
-    (let [api-class (hydra/find-class api range)]
-      (if (some? api-class)
-        (throw (Exception. (str "Class validation not implemented yet")))
+    (if (check-xsd-range range value)
+      nil
+      (->ValidationError :range-error [range value] (str "Error validating xsd range for range " range " and value " value)))
+    (let [validation (get api-validations range)]
+      (if (some? validation)
+        (validation api-validations value)
         (throw (Exception. (str "Cannot validate missing range class " range)))))))
 
 
@@ -72,14 +92,14 @@
                     required (-> % :args :supported-property :property-props ::hydra/required)
                     predicate (-> % :ret)]
                 (and (or (not required)
-                         (invalid? (predicate {})))
+                         (invalid? (predicate {} {})))
                      (or (not required)
-                         (invalid? (predicate {property []})))
-                     (invalid? (predicate {property [{"@value" 1} {"@value" 2}]}))))))
+                         (invalid? (predicate {} {property []})))
+                     (invalid? (predicate {} {property [{"@value" 1} {"@value" 2}]}))))))
 (defn parse-supported-link
   "Creates a specification for a hydra link"
   [api supported-property]
-  (fn [jsonld]
+  (fn [api-validations jsonld]
     (let [property (-> supported-property :property)
           is-optional (not (-> supported-property :property-props ::hydra/required))
           values (get jsonld property [])
@@ -100,6 +120,7 @@
                                                      supported-property
                                                      (str "Not string value for link (" value ") for property " property)))))))
 
+
 (s/fdef parse-plain-property
         :args (s/cat :api ::hydra/ApiDocumentation
                      :supported-property (s/and ::hydra/SupportedProperty
@@ -111,15 +132,15 @@
                     required  (-> % :args :supported-property :property-props ::hydra/required)
                     predicate (-> % :ret)]
                 (and (or (not required)
-                         (invalid? (predicate {})))
+                         (invalid? (predicate {} {})))
                      (or (not required)
-                         (invalid? (predicate {property []})))
-                     (invalid? (predicate {property [{"@value" 1} {"@value" 2}]})))
+                         (invalid? (predicate {} {property []})))
+                     (invalid? (predicate {} {property [{"@value" 1} {"@value" 2}]})))
                 true)))
 (defn parse-plain-property
   "Creates a specification for a hydra property"
   [api supported-property]
-  (fn [jsonld]
+  (fn [api-validations jsonld]
     (let [property (-> supported-property :property)
           range (-> supported-property :property-props ::hydra/range)
           is-optional (not (-> supported-property :property-props ::hydra/required))
@@ -131,11 +152,13 @@
                                 (->ValidationError :property-error supported-property (str "Missing mandatory property " property)))
         (> 1 (count values))  (->ValidationError :property-error supported-property (str "Cardinality error, more than one link value for property " property))
         :else                   (try
-                                  (if (check-range api range value)
-                                    nil
-                                    (->ValidationError :property-error supported-property (str "Not value (" value ") not in range (" range ") of property " property)))
+                                  (let [validation-result (check-range api-validations range value)]
+                                    (if (invalid? validation-result)
+                                      (->ValidationError :property-error validation-result (str "Not value (" value ") in range (" range ") of property " property))
+                                      nil))
                                   (catch Exception ex
                                     (->ValidationError :property-error supported-property (.getMessage ex))))))))
+
 
 (s/fdef parse-supported-property
         :args (s/cat :api ::hydra/ApiDocumentation
@@ -151,6 +174,7 @@
     (:is-template supported-property) (throw (Exception. "Not supported yet"))
     :else  (parse-plain-property api supported-property)))
 
+
 (s/fdef parse-supported-class
         :args (s/cat :api ::hydra/ApiDocumentation
                      :api-class ::hydra/SupportedClass)
@@ -159,16 +183,28 @@
   "Creates a specification for a hydra supported class"
   [api api-class]
   (let [validations (map #(parse-supported-property api %) (-> api-class :supported-properties))]
-    (fn [jsonld]
+    (fn [api-validations jsonld]
       ;(prn jsonld)
       (and (map? jsonld)
            (let [errors (->> validations
                              (map (fn [validation]
                                     ;(prn validation)
-                                    (validation jsonld)))
+                                    (validation api-validations jsonld)))
                              (filter #(not (nil? %))))]
              ;(prn "ERRORS?")
              ;(prn errors)
              (if (empty? errors)
                nil
                (->ValidationError :invalid-payload errors (str "Errors (" (count errors) ") found during validation"))))))))
+
+(s/fdef build-api-validations
+        :args (s/cat :apiDocumentation ::hydra/ApiDocumentation)
+        :ret ::ValidationsMap)
+(defn build-api-validations [api]
+  (let [classes (:supported-classes api)]
+    (reduce (fn [acc class]
+              (let [uri (-> class :common-props ::hydra/id)
+                    validation (parse-supported-class api class)]
+                (assoc acc uri validation)))
+            {}
+            classes)))
--- test/levanzo/schema_test.clj
+++ test/levanzo/schema_test.clj
@@ -31,7 +31,7 @@
                    :number number?
                    :boolean boolean?)))))
 
-(defn make-valid-payload-gen [class]
+(defn make-valid-payload-gen [class api]
   (tg/fmap (fn [properties]
              (let [m (->> properties
                           (map (fn [[k v]]
@@ -52,19 +52,18 @@
                                             (let [required (-> property-props ::hydra/required)
                                                   range (-> property-props ::hydra/range)]
                                               (tg/tuple (tg/return property)
-                                                        ;;(tg/return required)
-                                                        ;;boolean?
-                                                        ;;(tg/return is-link)
                                                         (if required
                                                           (if is-link
                                                             (s/gen ::jsonld-spec/uri)
                                                             (if (schema/xsd-uri? range)
                                                               (make-xsd-type-gen range)
-                                                              (s/gen ::jsonld-spec/uri)))
+                                                              (let [class (hydra/find-class api range)]
+                                                                ;; Careful! this will explode with cyclic APIs
+                                                                (make-valid-payload-gen range api))))
                                                           (tg/return nil)))))))]
                 (apply tg/tuple generators))))))
 
-(defn make-xsd-property-gen [type required]
+(defn make-literal-property-gen [type required]
   (tg/fmap (fn [property]
              (-> property
                  (assoc :operations [])
@@ -90,6 +89,7 @@
             (s/gen ::hydra/SupportedProperty)
             (s/gen ::hydra/Operation))))
 
+
 (defn make-properties-map-gen [max-properties]
   (tg/vector
    (tg/bind (s/gen (s/tuple #{:literal :link}
@@ -98,7 +98,7 @@
               (condp = kind
                 :literal (tg/bind (s/gen ::jsonld-spec/datatype)
                                   (fn [type]
-                                    (make-xsd-property-gen type required)))
+                                    (make-literal-property-gen type required)))
                 :link (tg/bind (s/gen ::jsonld-spec/uri)
                                (fn [uri]
                                  (make-link-property-gen uri required))))))
@@ -119,6 +119,76 @@
             (s/gen ::hydra/type)
             (make-properties-map-gen max-properties))))
 
+(defn make-api-tree-gen []
+  (tg/fmap
+   ;; we build the API now
+   (fn [{:keys [uri acc]}]
+     (hydra/api {::hydra/supported-classes acc
+                 ::hydra/entrypoint "/"
+                 ::hydra/entrypoint-class uri}))
+   ;; we generate a recursive tree of classes
+   (tg/recursive-gen (fn [g]
+                       ;; we collect all the classes generated and we add the
+                       ;; following class for this node
+                       (tg/fmap (fn [[u vs]]
+                                  (let [literals (->> vs
+                                                      (filter (fn [v] (= :literal (first v))))
+                                                      (map (fn [[_ l]] l)))
+                                        classes-links (filter (fn [v] (= :nested (first v))) vs)
+                                        links (map (fn [[_ link nested-class]] link) classes-links)
+                                        nested-classes (map (fn [[_ link nested-class]] nested-class) classes-links)
+                                        acc (->> classes-links
+                                                 (map (fn [[_ _ nested-class]]
+                                                        (:acc nested-class)))
+                                                 (apply concat))
+                                        class (hydra/class {::hydra/id u
+                                                            ::hydra/operations []
+                                                            ::hydra/supported-properties (concat literals links)})]
+                                    {:uri u :acc (concat acc [class])}))
+                                (tg/tuple
+                                 ;; class URI
+                                 (s/gen ::jsonld-spec/uri)
+                                 ;; supported-properties, including links to more classes
+                                 (tg/vector (tg/one-of
+                                             [
+                                              ;; nested literal
+                                              (tg/bind
+                                               (tg/tuple
+                                                (s/gen ::jsonld-spec/datatype)
+                                                tg/boolean)
+                                               (fn [[datatype required]]
+                                                 (tg/tuple
+                                                  (tg/return :literal)
+                                                  (make-literal-property-gen datatype required))))
+                                              ;; nested link
+                                              (tg/bind
+                                               (tg/tuple
+                                                ;; nested class
+                                                g
+                                                ;; potential datatype in case we get properties back
+                                                (s/gen ::jsonld-spec/datatype)
+                                                ;; link or nested literal?
+                                                tg/boolean
+                                                ;; required link
+                                                tg/boolean)
+                                               (fn [[class-map-or-uri datatype is-link required]]
+                                                 (if (string? class-map-or-uri)
+                                                   ;; end of recursion we have a link, we generate
+                                                   ;; yet another literal property
+                                                   (tg/tuple
+                                                    (tg/return :literal)
+                                                    (make-literal-property-gen datatype required))
+                                                   (let [class-uri (:uri class-map-or-uri)]
+                                                     (tg/tuple
+                                                      (tg/return :nested)
+                                                      (if is-link
+                                                        (make-link-property-gen class-uri required)
+                                                        (make-literal-property-gen class-uri required))
+                                                      (tg/return class-map-or-uri))))))
+                                              ])
+                                            ;; min 1 link, max 3
+                                            1 3))))
+                     (s/gen ::jsonld-spec/uri))))
 
 (deftest check-xsd-range-test
   (spec-utils/check-symbol `schema/check-xsd-range)
@@ -130,20 +200,63 @@
 (deftest parse-supported-link-test
   (spec-utils/check-symbol `schema/parse-supported-link))
 
-
  (deftest parse-plain-property-test
    (spec-utils/check-symbol `schema/parse-plain-property))
 
 (deftest parse-supported-property-test
    (spec-utils/check-symbol `schema/parse-supported-property))
 
-(deftest parse-supported-cass-test
-  (let [klasses (take 5 (gen/sample (make-class-gen "http://test.com/Test" 15)))]
+(deftest parse-supported-class-test
+  (let [klasses (take 3 (gen/sample (make-class-gen "http://test.com/Test" 15)))]
     (doseq [klass klasses]
-      (doseq [instance (gen/sample (make-valid-payload-gen klass) 10)]
-        (let [errors ((schema/parse-supported-class {} klass) instance)
+      (doseq [instance (gen/sample (make-valid-payload-gen klass nil) 10)]
+        (let [errors ((schema/parse-supported-class {} klass) {} instance)
               valid (nil? errors)]
           (when (not valid)
             (prn instance)
             (prn errors))
           (is valid))))))
+
+(deftest parse-supported-class-api-test
+  (println "Generating APIS...")
+  (doseq [api (take-last 3 (gen/sample (make-api-tree-gen) 15))]
+    (println "API!")
+    (s/valid? ::hydra/ApiDocumentation api)
+    (let [validations-map (schema/build-api-validations api)]
+      (doseq [klass (:supported-classes api)]
+        (println "CLASS!")
+        (doseq [instance (gen/sample (make-valid-payload-gen klass api) 10)]
+          (println "INSTANCE...")
+          (let [validation (get validations-map (-> klass :common-props ::hydra/id))
+                errors (validation validations-map instance)
+                valid (nil? errors)]
+            (when (not valid)
+              (prn instance)
+              (prn errors))
+            (is valid)))))))
+
+(comment
+
+
+  (prn (last (take 50 (gen/sample (tg/such-that not-empty (tg/vector tg/int))))))
+  (prn (last (take 50 (gen/sample (make-api-tree-gen)))))
+  (def gapi (last (take 50 (gen/sample (make-api-tree-gen)))))
+
+  (clojure.pprint/pprint gapi)
+  (last (take 1000 (gen/sample (tg/recursive-gen gen/vector (tg/recursive-gen gen/vector tg/int)) 10000)))
+  (def klass (first (gen/sample (make-class-gen "http://test.com/Test" 15))))
+
+  (def instance (first (gen/sample (make-valid-payload-gen klass))))
+
+  ((schema/parse-supported-class {} klass) instance)
+
+  (doseq []
+    (prn ))
+
+
+  (spec-utils/check-symbol `schema/parse-supported-property)
+
+  (gen/sample (tg/tuple (s/gen ::hydra/ApiDocumentation)
+                        (s/gen ::jsonld-spec/datatype)
+                        (s/gen ::jsonld-spec/jsonld-literal)) 1)
+  )

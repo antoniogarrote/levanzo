5a7c264d961e30431833242114b46d51b8b3c3fa
Antonio Garrote <antoniogarrote@gmail.com>
2017-01-23 21:10:12 +0000

    Adding examples directory to the project

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :monkeypatch-clojure-test false
+  :source-paths ["src" "examples"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [bidi "2.0.16"]
                  [commons-validator "1.5.1"]
